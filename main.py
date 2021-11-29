847731c3c2955f2bc1c64ac8d0fea758d64785f5
skewbekkebweks <maihail.glazov2015@yandex.ru>
2021-11-29 21:47:22 +0300

    complete

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
+from random import randint
 
 
 class MyWidget(QMainWindow):
@@ -9,9 +11,26 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.run)
+        self.flag = False
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(5):
+            x = randint(100, 600)
+            y = randint(0, 400)
+            r = randint(50, 200)
+            qp.drawEllipse(x, y, r, r)
 
     def run(self):
-        pass
+        self.flag = True
+        self.repaint()
 
 
 def except_hook(cls, exception, traceback):
